04c8b20b288547e65fde6c6950961add43fcd1f1
jerem2802 <creamedoc@gmail.com>
2024-10-16 23:50:34 +0200

    [feat]: well done

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import "dotenv/config";
-import {say} from "cowsay";
+import { say } from "cowsay";
 
-
-
-console.log (
+console.log(
 	say({
 		text: `Je m'apelle ${process.env.NAME} du campus ${process.env.CAMPUS}!`,
 		e: "oO",
 		T: "U ",
 	}),
 );
- 
